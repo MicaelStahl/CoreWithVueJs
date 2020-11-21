41569f384bd946a1b69b88e876d7272bf260532b
Micael Ståhl <micael_stahl96@hotmail.com>
2020-11-22 00:38:52 +0100

    La till en Token Service

---
+++ CoreWithVueJs/Business/Factories/JwtTokenService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace CoreWithVueJs.Business.Factories
+{
+    public class JwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private const string URL = "https://localhost:54051";
+
+        public string GenerateToken(string userID)
+        {
+            string mySecret = _configuration["SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret));
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userID)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                Issuer = URL,
+                Audience = URL,
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public bool ValidateCurrentToken(string token)
+        {
+            string mySecret = _configuration["SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret));
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                if (tokenHandler.CanValidateToken)
+                {
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidIssuer = URL,
+                        ValidAudience = URL,
+                        IssuerSigningKey = key
+                    }, out SecurityToken _);
+
+                    return true;
+                }
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+
+        public string GetClaim(string token, string claimType)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.ReadJwtToken(token);
+
+            return securityToken.Claims.FirstOrDefault(claim => claim.Type == claimType)?.Value;
+        }
+    }
+}
